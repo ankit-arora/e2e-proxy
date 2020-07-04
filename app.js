51175475e9a6f441935b0313d7896c1f4d6e469b
Ankit Arora <ankit.j.arora@gmail.com>
2020-07-04 14:39:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var createError = require('http-errors');
+var express = require('express');
+var proxy = require('express-http-proxy');
+
+var app = express();
+
+app.use(
+    '/foo',
+    proxy('my-test-hdfc-life-website-foo.s3-website.ap-south-1.amazonaws.com', {})
+);
+
+app.use(
+    '/bar',
+    proxy('my-test-hdfc-life-website-bar.s3-website.ap-south-1.amazonaws.com', {})
+);
+
+app.use(
+    '/',
+    proxy('my-test-hdfc-life-website-bar.s3-website.ap-south-1.amazonaws.com', {})
+);
+
+module.exports = app;
